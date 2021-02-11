33b62c51a9f9641e9b9f2dc95fff9752d4702b05
nocturnalarchives <64667374+nocturnalarchives@users.noreply.github.com>
2021-02-11 17:46:29 -0500

    isolated public and private

--- wp-content/plugins/atlasws-private-plugin/include-h-speech-match.php
+++
@@ -1,5 +0,0 @@
-<?
-
-$hsppech_check = "/(?i)9-11|africa|bacon-haters|black|diversity|holocaust|islam|jesus|jew|lgbtq|notice-things|race|racial|racist|religous-group|shapiro|slavery|slave|splc|trans|zog/";
-
-?>
